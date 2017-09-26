560fa8dce5257704a92ffafa1ba308467af2541f
David Ramirez <davidsramirez11@gmail.com>
2017-09-26 14:22:19 -0700

    make http request to the api to get all todos and display them to the html:

--- client/app/components/todos.component.ts
+++ client/app/components/todos.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {TodoService} from '../services/todo.service';
+import {Todo} from '../Todo';
+
+
 
 @Component({
 	moduleId: module.id,
 	selector: 'todos',
 	templateUrl: 'todos.component.html'
 })
-export class TodosComponent { }
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  
+  constructor(private _todoService: TodoService){
+    
+  }
+  
+  ngOnInit(){
+    this.todos = [];
+    this._todoService.getTodos()
+      .subscribe(todos => {
+        this.todos = todos;
+      });
+  }
+ }
